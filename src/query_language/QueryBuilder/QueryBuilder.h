f8429742ab2687f9219771fa72c22c501d544bdd
PixelQuasar <47022046+PixelQuasar@users.noreply.github.com>
2024-11-09 19:50:52 +0300

    queryBuilder partial definition

--- src/query_language/QueryBuilder/QueryBuilder.h
+++ src/query_language/QueryBuilder/QueryBuilder.h
@@ -1,16 +1,32 @@
 //
 // Created by QUASARITY on 06.11.2024.
 //
+#include <string>
+#include <vector>
 
 #ifndef DATABASE_CONTROLLER_HSE_QUERYBUILDER_H
 #define DATABASE_CONTROLLER_HSE_QUERYBUILDER_H
 
 namespace database {
-
-    class QueryBuilder {
+    class Context {
 
     };
 
+    class QueryBuilder {
+    public:
+        explicit QueryBuilder (std::vector<std::string>& tokens) : m_tokens(tokens) {
+
+        }
+
+        static QueryBuilder BuildFromString(std::string& string_query) {
+            std::vector<std::string> new_tokens = {};
+
+            return QueryBuilder(new_tokens);
+        }
+    private:
+        std::vector<std::string> m_tokens = {};
+        Context m_query_context = {};
+    };
 } // database
 
 #endif //DATABASE_CONTROLLER_HSE_QUERYBUILDER_H
