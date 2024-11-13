c8b1ab0ccc4b854b38ab3a1586c8836eece95223
PixelQuasar <47022046+PixelQuasar@users.noreply.github.com>
2024-11-13 21:37:32 +0300

    some minor refactoring and main.cpp basic examplefgsdfggfd

--- main.cpp
+++ main.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
 #include "src/database/Database/Database.h"
+#include "src/database/Schema/Schema.h"
+
+class User : public database::Schema {
+public:
+    std::string email;
+    std::string name;
+    int age;
+};
 
 int main() {
     auto db = new database::Database();
+
+    std::string query = " SELECT name FROM users";
+
+    auto names = db->exec<database::Schema>(query);
 
     std::cout << "hello world!" << std::endl;
     return 0;
--- src/database/Database/Database.h
+++ src/database/Database/Database.h
@@ -1,20 +1,27 @@
 //
 // Created by QUASARITY on 06.11.2024.
 //
-#include <iostream>
-
 #ifndef DATABASE_CONTROLLER_HSE_DATABASE_H
 #define DATABASE_CONTROLLER_HSE_DATABASE_H
+
+#include <iostream>
+#include <vector>
+#include "../Schema/Schema.h"
 
 namespace database {
 
     class Database {
     public:
         explicit Database() {
-            std::cout << "hello" << std::endl;
+
+        }
+
+        template<class T>
+        std::vector<std::unique_ptr<T>> exec(std::string& query_str) {
+            std::vector<std::unique_ptr<Schema>> result = {};
+            return result;
         }
     };
-
 } // database
 
 #endif //DATABASE_CONTROLLER_HSE_DATABASE_H
